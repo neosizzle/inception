0f9ecbf7f5c305e962eafa3e9ed949cd862b990a
neosizzle <junahnngd1115@gmail.com>
2022-03-11 13:40:47 +0800

    wp fucks me up

--- srcs/requirements/mariadb/conf/db-setup.sh
+++ srcs/requirements/mariadb/conf/db-setup.sh
@@ -1,12 +1,21 @@
-#start mysql
+# copy to custom data directory and grant all permissions if directory not exist
+cp -n -r /var/lib/mysql/* /docker_mysqldata # -n is no overwrite, -r is recursive
+chmod -R 777 /docker_mysqldata
+
+# start mysql
 /etc/init.d/mysql start
 
-#create users and grand privelleges
-mysql -Bse "CREATE USER '$MYSQL_ADMIN'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -Bse "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -Bse "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_ADMIN'@'localhost';"
-mysql -Bse "GRANT SELECT ON * . * TO '$MYSQL_USER'@'localhost';"
-mysql -Bse "SELECT user from mysql.user"
+#create users and grant privelleges
+echo "CREATE USER IF NOT EXISTS  '$MYSQL_ADMIN'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_ADMIN'@'localhost';" | mysql
+echo "GRANT SELECT ON * . * TO '$MYSQL_USER'@'localhost';" | mysql
+echo "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DATABASE;" | mysql
+echo "USE $WORDPRESS_DATABASE;CREATE TABLE IF NOT EXISTS data (value VARCHAR(20));" | mysql
+echo "SELECT user from mysql.user;" | mysql
+
+# stop mysql
+/etc/init.d/mysql stop
 
 #run sql on foreground
-mysqld_safe
+mysqld_safe --skip-syslog --log-error=logs.log
---
+++ srcs/requirements/wordpress/wp-setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+tail -f
