b5d16cd80f15d569922946dddcf326cee6d04cf3
neosizzle <junhanngd1115@gmail.com>
2022-03-14 14:15:41 +0800

    added else statement on wp setup

--- srcs/requirements/wordpress/wp-setup.sh
+++ srcs/requirements/wordpress/wp-setup.sh
@@ -3,7 +3,8 @@ while ! mysql -h nszl-mariadb -u $MYSQL_ADMIN -p$MYSQL_ADMIN_PASSWORD $WORDPRESS
     sleep 3
 done
 
-if [ ! -d "/usr/local/bin/wp" ]; then
+if [ ! -d "/wp_site/wordpress" ];
+	then
   	#get wordpress cli 
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -19,6 +20,9 @@ if [ ! -d "/usr/local/bin/wp" ]; then
 	wp config create --dbhost=nszl-mariadb --dbname=$WORDPRESS_DATABASE --dbuser=$MYSQL_ADMIN --dbpass="$MYSQL_ADMIN_PASSWORD" --allow-root
 	chmod 644 wp-config.php
 	wp core install --url=localhost --title="Your Blog Title" --admin_name=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=you@example.com --allow-root
+
+	else
+	echo "Wordpress is already installed.."
 fi
 
 #make directory to contain php fpm socket
